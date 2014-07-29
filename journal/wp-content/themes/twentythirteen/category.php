613c36dce9a71809e9e07cf74bb17a7a0d215dfb
torstenbergler <torsten@nomoresleep.net>
2014-07-29 20:05:36 +0200

    Fixed Category & Tag View

--- journal/wp-content/themes/twentythirteen/category.php
+++ journal/wp-content/themes/twentythirteen/category.php
@@ -11,56 +11,50 @@ get_header('magento'); ?>
         <div class="main-container col1-layout">
             <div class="main">
 <?php
-            $args = array(
-			    'posts_per_page' => 9,
-			    'offset' => 0,
-			    'category' => the_category_ID(FALSE),
-			    'orderby' => 'post_date',
-			    'order' => 'DESC',
-			    'post_type' => 'post',
-			    'post_status' => 'publish',
-			    'suppress_filters' => true
-			);
-			$posts = wp_get_recent_posts($args);
+   //          $args = array(
+			//     'posts_per_page' => -1,
+			//     'offset' => 0,
+			//     'tag_id' => get_query_var('tag_id')
+			// );
+			// $posts = new WP_Query($args);
 
 			/* ------------------------- Post last new ------------------------- */
 
-			if ( !empty($posts) AND is_array($posts) AND count($posts) )
-			{
-				$category = get_the_category();
+			global $query_string;
+			query_posts( $query_string . '&posts_per_page=-1' );
+
+			if ( have_posts() ) :
 				echo '
 				<div class="home-page-recent-articles page-category">
 					<div class="page-category-title">
 						<!--<div class="category-label">'.__('Category').'</div>-->
-						<div class="category-value">'.qtrans_use(mage_get_language(),$category[0]->name).'</div>
+						<div class="category-value">'.qtrans_use(mage_get_language(),single_cat_title( '', false )).'</div>
 					</div>
 					<div class="row">';
-				foreach ($posts as $post)
-				{
-					$caption = get_post_custom($post['ID']);
+				while ( have_posts() ) : the_post();
+					$caption = get_post_custom(get_the_ID());
 					$caption = ( isset($caption['wpcf-post-description']) AND !empty($caption['wpcf-post-description']) AND is_array($caption['wpcf-post-description']) AND count($caption['wpcf-post-description']) ) ? $caption['wpcf-post-description'][0] : '';
 					echo '
 					<div class="small-12 medium-4 large-4 large-recent-articles columns">
 						<div class="home-page-recent-articles-image">
-							<img src="'.(dirname(get_site_url()).'/lib/timthumb.php?src='.wp_get_attachment_url( get_post_thumbnail_id($post['ID']) ).'&w=412&h=252').'" />
-							<a class="a-view-more" href="'.qtrans_convertURL(get_permalink($post['ID']), mage_get_language()).'"><span>'.__('View').'</span></a>
+							<img src="'.(dirname(get_site_url()).'/lib/timthumb.php?src='.wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) ).'&w=412&h=252').'" />
+							<a class="a-view-more" href="'.qtrans_convertURL(get_permalink(get_the_ID()), mage_get_language()).'"><span>'.__('View').'</span></a>
 						</div>
 						<div class="home-page-recent-articles-description">
-							<div class="title"><a href="'.qtrans_convertURL(get_permalink($post['ID']), mage_get_language()).'">'.qtrans_use(mage_get_language(),get_the_title($post['ID'])).'</a></div>
+							<div class="title"><a href="'.qtrans_convertURL(get_permalink(get_the_ID()), mage_get_language()).'">'.qtrans_use(mage_get_language(),get_the_title(get_the_ID())).'</a></div>
 							<div class="description">'.$caption.'</div>
 							<div class="bottom">
-								<div class="bottom-view"><a href="'.qtrans_convertURL(get_permalink($post['ID']), mage_get_language()).'">'.__('View post').'</a></div>
-								<div class="bottom-date">['.date("d-m-Y", strtotime($post['post_date'])).']</div>
+								<div class="bottom-view"><a href="'.qtrans_convertURL(get_permalink(get_the_ID()), mage_get_language()).'">'.__('View post').'</a></div>
+								<div class="bottom-date">['.date("d-m-Y", strtotime(get_the_date())).']</div>
 							</div>
 						</div>
 					</div>';
-				}
-
+				endwhile;
 				echo'
 					</div>
 				</div>
 				';
-			}
+			endif;
 ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
--- journal/wp-content/themes/twentythirteen/tag.php
+++ journal/wp-content/themes/twentythirteen/tag.php
@@ -1,43 +1,61 @@
 <?php
 /**
- * The template for displaying Tag pages
- *
- * Used to display archive-type pages for posts in a tag.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
+ * The template for displaying all single posts
  *
  * @package WordPress
  * @subpackage Twenty_Thirteen
  * @since Twenty Thirteen 1.0
  */
 
-get_header(); ?>
+get_header('magento'); ?>
+        <div class="main-container col1-layout">
+            <div class="main">
+<?php
+   //          $args = array(
+			//     'posts_per_page' => -1,
+			//     'offset' => 0,
+			//     'tag_id' => get_query_var('tag_id')
+			// );
+			// $posts = new WP_Query($args);
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+			/* ------------------------- Post last new ------------------------- */
 
-		<?php if ( have_posts() ) : ?>
-			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Tag Archives: %s', 'twentythirteen' ), single_tag_title( '', false ) ); ?></h1>
+			global $query_string;
+			query_posts( $query_string . '&posts_per_page=-1' );
 
-				<?php if ( tag_description() ) : // Show an optional tag description ?>
-				<div class="archive-meta"><?php echo tag_description(); ?></div>
-				<?php endif; ?>
-			</header><!-- .archive-header -->
-
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
-
-			<?php twentythirteen_paging_nav(); ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
-
+			if ( have_posts() ) :
+				echo '
+				<div class="home-page-recent-articles page-category">
+					<div class="page-category-title">
+						<!--<div class="category-label">'.__('Category').'</div>-->
+						<div class="category-value">'.qtrans_use(mage_get_language(),single_tag_title( '', false )).'</div>
+					</div>
+					<div class="row">';
+				while ( have_posts() ) : the_post();
+					$caption = get_post_custom(get_the_ID());
+					$caption = ( isset($caption['wpcf-post-description']) AND !empty($caption['wpcf-post-description']) AND is_array($caption['wpcf-post-description']) AND count($caption['wpcf-post-description']) ) ? $caption['wpcf-post-description'][0] : '';
+					echo '
+					<div class="small-12 medium-4 large-4 large-recent-articles columns">
+						<div class="home-page-recent-articles-image">
+							<img src="'.(dirname(get_site_url()).'/lib/timthumb.php?src='.wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) ).'&w=412&h=252').'" />
+							<a class="a-view-more" href="'.qtrans_convertURL(get_permalink(get_the_ID()), mage_get_language()).'"><span>'.__('View').'</span></a>
+						</div>
+						<div class="home-page-recent-articles-description">
+							<div class="title"><a href="'.qtrans_convertURL(get_permalink(get_the_ID()), mage_get_language()).'">'.qtrans_use(mage_get_language(),get_the_title(get_the_ID())).'</a></div>
+							<div class="description">'.$caption.'</div>
+							<div class="bottom">
+								<div class="bottom-view"><a href="'.qtrans_convertURL(get_permalink(get_the_ID()), mage_get_language()).'">'.__('View post').'</a></div>
+								<div class="bottom-date">['.date("d-m-Y", strtotime(get_the_date())).']</div>
+							</div>
+						</div>
+					</div>';
+				endwhile;
+				echo'
+					</div>
+				</div>
+				';
+			endif;
+?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_footer('magento');?>
